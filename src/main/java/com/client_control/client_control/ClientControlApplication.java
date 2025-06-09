524b5c4c0da5b2323047169a7b84e6ec205ed303
Leonardo Campos <leocampos.995@gmail.com>
2025-06-09 16:44:17 -0300

    first commit

---
+++ src/main/java/com/client_control/client_control/ClientControlApplication.java
@@ -0,0 +1,13 @@
+package com.client_control.client_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/client_control/client_control/ClientControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client_control.client_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
