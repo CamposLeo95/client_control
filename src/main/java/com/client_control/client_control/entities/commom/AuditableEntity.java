d1ec518c94e8a44a9a628b52effc133347ddc821
Leonardo Campos <leocampos.995@gmail.com>
2025-06-10 17:48:22 -0300

    feat:created Commom and extends classes

---
+++ src/main/java/com/client_control/client_control/entities/commom/AuditableEntity.java
@@ -0,0 +1,41 @@
+package com.client_control.client_control.entities.commom;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+
+import java.time.LocalDateTime;
+
+// @MappedSuperclass: permite que outras entidades herdem os campos e métodos sem criar uma tabela separada.
+@MappedSuperclass
+public abstract class AuditableEntity {
+
+    @Column(name = "createdAt", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updatedAt")
+    private LocalDateTime updatedAt;
+
+    //@PrePersist: executa antes do INSERT.
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    //@PreUpdate: executa antes do UPDATE.
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt(){
+        return updatedAt;
+    }
+
+}
